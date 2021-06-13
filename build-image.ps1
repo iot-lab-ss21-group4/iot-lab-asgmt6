3ebfc4e3b62d527b84447c8a3b240784fccd04b8
Deniz Etkar <denizetkar@gmail.com>
2021-06-13 14:57:44 +0000

    improved separation of concern between 'docker build' and 'docker-compose'

--- build-image.ps1
+++ build-image.ps1
@@ -1,8 +1,22 @@
 param(
     [string] $DeploymentType = "edge",
     [string] $AppName = "forecaster",
+    [string] $EnvFilePath = ".env.secret.edge.forecaster",
     [Parameter(Mandatory = $true)] [string] $DockerId
 )
 
-docker build -f docker-apps/${DeploymentType}/${AppName}/Dockerfile . -t ${DockerId}/iotlab:${DeploymentType}-${AppName}
-docker push ${DockerId}/iotlab:${DeploymentType}-${AppName}
+# Load the build arguments for 'docker build' and the corresponding Dockerfile
+$build_args = Get-Content -Raw -Path $EnvFilePath | ConvertFrom-StringData
+# Convert all keys to lowercase letters before adding as a build argument
+$build_arg_str = ""
+foreach ($key in $build_args.PSBase.Keys) {
+    $build_arg_str += " --build-arg $($key.ToString().ToLowerInvariant())=$($build_args[$key])"
+}
+
+$dockerfile_path = "docker-apps/${DeploymentType}/${AppName}/Dockerfile"
+$build_context_path = "."
+$build_tag = "${DockerId}/iotlab:${DeploymentType}-${AppName}"
+$docker_build_cmd = "docker build -f ${dockerfile_path} ${build_context_path} -t ${build_tag}"
+$docker_build_cmd += $build_arg_str
+Invoke-Expression -Command $docker_build_cmd
+docker push ${build_tag}
