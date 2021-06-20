8c1f4a00e3fa7bb9a34f2286022dbaf758150648
Maximilian Wiesholler <maximilian.wiesholler@tum.de>
2021-06-20 13:18:48 +0200

    better naming

--- docker-apps/edge/forecaster/app/edge/util/accuracy.py
+++ docker-apps/edge/forecaster/app/edge/util/accuracy.py
@@ -19,42 +19,42 @@ class AccuracyCalculator:
 
     def compute_accuracy_metrics(self, real_counts: List[int], forecasts: List[int], forecasts_train: List[int]):
         return Accuracy(
-            mae=AccuracyCalculator.compute_mae(real_counts, forecasts),
-            rmse=AccuracyCalculator.compute_rmse(real_counts, forecasts),
-            mape=AccuracyCalculator.compute_rmse(real_counts, forecasts),
-            smape=AccuracyCalculator.compute_smape(real_counts, forecasts),
-            mase=AccuracyCalculator.compute_mase(real_counts, forecasts, forecasts_train),
-            ias=AccuracyCalculator.compute_ias(real_counts, forecasts, self.ias_quantile, self.ias_sig_alpha),
+            mae=AccuracyCalculator.mean_absolute_error(real_counts, forecasts),
+            rmse=AccuracyCalculator.root_mean_squared_error(real_counts, forecasts),
+            mape=AccuracyCalculator.root_mean_squared_error(real_counts, forecasts),
+            smape=AccuracyCalculator.symmetric_mean_absolute_percentage_error(real_counts, forecasts),
+            mase=AccuracyCalculator.mean_absolute_scaled_error(real_counts, forecasts, forecasts_train),
+            ias=AccuracyCalculator.interval_accuracy_score(real_counts, forecasts, self.ias_quantile, self.ias_sig_alpha),
         )
 
     @staticmethod
-    def compute_mae(ys_real: List[int], ys_pred: List[int]) -> float:
+    def mean_absolute_error(ys_real: List[int], ys_pred: List[int]) -> float:
         errors = [abs(y - y_hat) for y, y_hat in zip(ys_real, ys_pred)]
         return sum(errors) / len(errors)
 
     @staticmethod
-    def compute_rmse(ys_real: List[int], ys_pred: List[int]) -> float:
+    def root_mean_squared_error(ys_real: List[int], ys_pred: List[int]) -> float:
         errors = [(y - y_hat) ** 2 for y, y_hat in zip(ys_real, ys_pred)]
         return sqrt(sum(errors) / len(errors))
 
     @staticmethod
-    def compute_mape(ys_real: List[int], ys_pred: List[int]) -> float:
+    def mean_absolute_percentage_error(ys_real: List[int], ys_pred: List[int]) -> float:
         errors = [abs(100 * (y - y_hat) / y_hat) for y, y_hat in zip(ys_real, ys_pred)]
         return sum(errors) / len(errors)
 
     @staticmethod
-    def compute_smape(ys_real: List[int], ys_pred: List[int]) -> float:
+    def symmetric_mean_absolute_percentage_error(ys_real: List[int], ys_pred: List[int]) -> float:
         errors = [200 * abs(y - y_hat) / (y + y_hat) for y, y_hat in zip(ys_real, ys_pred)]
         return sum(errors) / len(errors)
 
     @staticmethod
-    def compute_mase(ys_real: List[int], ys_pred: List[int], ys_train_pred: List[int]) -> float:
-        base = AccuracyCalculator.compute_mae(ys_train_pred[1:], ys_train_pred[:-1])
-        forecasts_error_mean = AccuracyCalculator.compute_mae(ys_real, ys_pred)
+    def mean_absolute_scaled_error(ys_real: List[int], ys_pred: List[int], ys_train_pred: List[int]) -> float:
+        base = AccuracyCalculator.mean_absolute_error(ys_train_pred[1:], ys_train_pred[:-1])
+        forecasts_error_mean = AccuracyCalculator.mean_absolute_error(ys_real, ys_pred)
         return forecasts_error_mean / base
 
     @staticmethod
-    def compute_ias(ys_real: List[int], ys_pred: List[int], quantile: float, sig_alpha: float) -> float:
+    def interval_accuracy_score(ys_real: List[int], ys_pred: List[int], quantile: float, sig_alpha: float) -> float:
         mean = sum(ys_real) / len(ys_real)
         st_dev = sqrt(sum([(y - mean) ** 2 for y in ys_real]) / (len(ys_real) - 1))
         lb = mean - quantile * st_dev
