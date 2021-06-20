e33e2da03bfc2a464d4a57002bf860d1651168a9
Maximilian Wiesholler <maximilian.wiesholler@tum.de>
2021-06-20 20:47:28 +0200

    reformat

--- docker-apps/edge/forecaster/app/test/test_forecast_combine_strategy.py
+++ docker-apps/edge/forecaster/app/test/test_forecast_combine_strategy.py
@@ -6,30 +6,22 @@ from edge.util.forecast_combiner import ForecastCombiner
 
 
 class TestForecastCombineStrategy(unittest.TestCase):
-
     def setUp(self):
         accCalculator = AccuracyCalculator()
         timestamp = 42
-        real_counts = [1,1,1,2,3,4,5,6,7,8,7,8,9]
+        real_counts = [1, 1, 1, 2, 3, 4, 5, 6, 7, 8, 7, 8, 9]
         self.forecast_model_1 = 8
         self.forecast_model_2 = 7
-        forecasts_1 = [1,1,1,1,2,4,5,6,7,8,8,8,self.forecast_model_1]
-        forecasts_2 = [1,2,3,4,5,6,7,7,7,7,7,7,self.forecast_model_2]
-        self.accuracy_1 = accCalculator.compute_accuracy_metrics(
-            real_counts=real_counts,
-            forecasts=forecasts_1
-        )
-        self.accuracy_2 = accCalculator.compute_accuracy_metrics(
-            real_counts=real_counts,
-            forecasts=forecasts_2
-        )
+        forecasts_1 = [1, 1, 1, 1, 2, 4, 5, 6, 7, 8, 8, 8, self.forecast_model_1]
+        forecasts_2 = [1, 2, 3, 4, 5, 6, 7, 7, 7, 7, 7, 7, self.forecast_model_2]
+        self.accuracy_1 = accCalculator.compute_accuracy_metrics(real_counts=real_counts, forecasts=forecasts_1)
+        self.accuracy_2 = accCalculator.compute_accuracy_metrics(real_counts=real_counts, forecasts=forecasts_2)
         self.model_1 = "better_model"
         self.model_2 = "weaker_model"
         self.model_results = {
             self.model_1: (timestamp, self.forecast_model_1, self.accuracy_1),
-            self.model_2: (timestamp, self.forecast_model_2, self.accuracy_2)
+            self.model_2: (timestamp, self.forecast_model_2, self.accuracy_2),
         }
-
 
     def test_select_most_accurate_strategy(self):
         combiner = ForecastCombiner(SelectMostAccurateStrategy())
