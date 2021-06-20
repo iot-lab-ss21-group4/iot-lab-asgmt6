18e6e5182016fbe134255f287ee4aff0496a1373
Deniz Etkar <denizetkar@gmail.com>
2021-06-20 18:28:19 +0000

    added the skeleton code for tracking the model forecast values and providing value pairs for accuracy calculations

--- docker-apps/edge/forecaster/app/edge/thread/forecast_evaluator_thread.py
+++ docker-apps/edge/forecaster/app/edge/thread/forecast_evaluator_thread.py
@@ -1,4 +1,5 @@
 import threading
+from collections import defaultdict
 from queue import Queue
 from typing import Dict, List, Tuple
 
@@ -32,6 +33,20 @@ class ForecastEvaluatorThread(threading.Thread):
         self.accuracy_calculator = accuracy_calculator
         self.forecast_combiner = forecast_combiner
 
+        self.target_buffer: List[int] = []
+        self.forecast_buffer: Dict[str, List[int]] = defaultdict(list)
+
+    def track_model_forecasts(self, model_type: str, t: int, y: int):
+        # TODO: update the target and forecast buffers and leave them in a valid state!
+        pass
+
+    def get_target_and_forecast_pairs(self) -> Tuple[List[int], List[int]]:
+        # TODO: assuming the target and forecast buffers are in a valid state, return the longest list of target
+        # and forecast values such that targets[0] <= forecasts[0] AND forecasts[-1] <= targets[-1].
+        targets: List[int] = []
+        forecasts: List[int] = []
+        return targets, forecasts
+
     def run(self):
         evaluation_rounds: List[Dict[str, Tuple[int, int, Accuracy]]] = []
         while True:
@@ -45,17 +60,20 @@ class ForecastEvaluatorThread(threading.Thread):
             if round_index == len(evaluation_rounds):
                 evaluation_rounds.append(dict())
 
-            # TODO: check if accuracy computation (online evaluation) is possible
-            # TODO: how to get newest observation? continuous polling?
-            # TODO: compute accuracies and publish results
-            # TODO: remove oldest forecast element if evaluation was possible
-            # TODO: feed with real data
-            accuracy = self.accuracy_calculator.compute_accuracy_metrics(real_counts=[1, 2, 3], forecasts=[2, 3, 4])
-            self.platform_sensor_publisher.publish(
-                ForecastEvaluatorThread.ACCURACY_SENSOR_PREFIX + model_type.upper(), t, accuracy._asdict()
-            )
-            evaluation_rounds[round_index][model_type] = (t, y, accuracy)
+            # Get the target and forecast values ready for accuracy metric calculations
+            self.track_model_forecasts(model_type, t, y)
+            # Get the target and forecast value pairs
+            real_counts, forecasts = self.get_target_and_forecast_pairs()
+            assert len(real_counts) == len(forecasts), "Target and forecast value pairs must be of equal size!"
+            if len(real_counts) > 0:
+                # Perform accuracy metric calculations, since we have some target and forecast values
+                accuracy = self.accuracy_calculator.compute_accuracy_metrics(real_counts=[1, 2, 3], forecasts=[2, 3, 4])
+                self.platform_sensor_publisher.publish(
+                    ForecastEvaluatorThread.ACCURACY_SENSOR_PREFIX + model_type.upper(), t, accuracy._asdict()
+                )
+                evaluation_rounds[round_index][model_type] = (t, y, accuracy)
 
+            # Check if all models submitted their forecasts for this evaluation round.
             if len(evaluation_rounds[round_index]) >= ForecastEvaluatorThread.MODEL_COUNT:
                 # Note that 't' for all models at this evaluation round must be the same!
                 # TODO: Find best y using strategy
