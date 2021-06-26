48556ebbd90d44fdee581ace037b4c4504903eae
Deniz Etkar <denizetkar@gmail.com>
2021-06-26 20:24:46 +0000

    fixed time drift in timer thread

--- docker-apps/edge/forecaster/app/edge/thread/timer_thread.py
+++ docker-apps/edge/forecaster/app/edge/thread/timer_thread.py
@@ -17,13 +17,15 @@ class TimerThread(threading.Thread):
         self.forecast_period = forecast_period
         self.forecast_dt = forecast_dt
 
-        self._next_forecast_time = time.time() + self.forecast_period
+        self._now = time.time()
+        self._next_forecast_time = self._now + self.forecast_period
 
     def run(self):
         while True:
-            pred_time = int(time.time()) + self.forecast_dt
+            pred_time = int(self._now) + self.forecast_dt
             logging.info("Send pred time {} to forecasters.".format(str(pred_time)))
             for q in self.event_out_qs:
                 q.put(pred_time)
+            self._now = self._next_forecast_time
             time.sleep(max(0.0, self._next_forecast_time - time.time()))
             self._next_forecast_time += self.forecast_period
